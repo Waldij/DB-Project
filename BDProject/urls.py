27fbddd943172d9f9a88b5019b98dd57845d9df1
Waldij <36505353+Waldij@users.noreply.github.com>
2020-05-28 19:53:17 +0800

    Update urls.py

--- BDProject/urls.py
+++ BDProject/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 
 urlpatterns = [
+    path('', include('articles.urls')),
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
